61571f62c366b84946a8226a002f8f214e9b01f9
MaxChang3 <maxcfoo@gmail.com>
2023-03-23 16:45:04 +0800

    chore: fix dependencies bug

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
+const path = require('path')
+const moduleAlias = require('module-alias')
+moduleAlias.addAlias('@', path.join(__dirname, '../node_modules/rsshub/lib'))
+
 const config = require('rsshub/lib/config')
 
 config.set({
